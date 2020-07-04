4093e63cbfb05687fc93fd24925532da6e632aac
Trevor Haralson <trevor.haralson@gmail.com>
2020-07-04 10:55:50 -0700

    First working commit.

---
+++ get-all-chores/app.js
@@ -0,0 +1,45 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+//let response;
+//const url = require()
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+const { Client } = require('pg');
+
+exports.lambdaHandler = async(event) => {
+    const client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      database: process.env.POSTGRES_DBNAME 
+    });
+    client
+      .connect()
+      .then((response) => {
+        console.log("connected");
+      })
+      .catch((err) => {
+        console.log("error");
+        console.log(err);
+      });
+    
+    return new Promise(
+      (resolve, reject) => client
+      .query(`SELECT * FROM public."Users"`)
+      .then((res) => resolve(res.rows))
+      .catch((err) => reject(Error(err)))
+      .then(() => client.end())
+      );
+};
+
