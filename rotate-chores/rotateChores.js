3f82ae47570ed11f95a7b0ac629e53ed252d4609
Trevor Haralson <trevor.haralson@gmail.com>
2020-07-27 23:35:53 -0700

    lay groundwork for function.

---
+++ rotate-chores/rotateChores.js
@@ -0,0 +1,62 @@
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ *
+ */
+
+const { Client } = require('pg')
+
+exports.lambdaHandler = async function (event) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DBNAME
+  })
+  await rotateChores(client)
+}
+
+async function rotateChores (client) {
+  return client.connect()
+    .then(() => {
+      console.log('Successfully connected to the DB!')
+      return getChoreAssignments(client)
+    })
+    .then((choreAssignment) => {
+      return rotateChoreAssignment(choreAssignment)
+    })
+    .then((newChoreAssignment) => {
+      setChoreAssignment(newChoreAssignment)
+    })
+    .catch((err) => {
+      disconnectDb(client)
+      console.log(`There was an error :( -> ${err}`)
+    })
+}
+function setChoreAssignment (client) {
+
+}
+function rotateChoreAssignment (client) {
+
+}
+function getChoreAssignments (client) {
+  return client.query(`
+    SELECT c.chore_id, c.assigned_to 
+    FROM dev."Chores" c
+  `)
+}
+
+function disconnectDb (client) {
+  return client
+    .end()
+    .then(() => {
+      console.log('Successfully disconnected from DB')
+    })
+}
